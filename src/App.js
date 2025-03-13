15377c30c95825c314543343d401e47f51477225
TheintTheintSH <theinttheintsoehtet.edka@gmail.com>
2025-03-13 16:00:55 +0630

    react browserrouter-router, routes and route

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "../src/components/Dashboard";
+import Recommendations from "../src/components/Recommendations";
+import Podcasts from "../src/components/Podcasts";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="text-3xl text-center py-6 my-2">useContext</h1>
+
+      <Router>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/recommendations" element={<Recommendations />} />
+          <Route path="/podcasts" element={<Podcasts />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
